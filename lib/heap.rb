57809b6dd96b097b6c26dc3f8dab58492b6c7d1f
Richard Ma <rich.maaa@gmail.com>
2018-09-06 11:53:17 -0700

    Finish head

--- lib/heap.rb
+++ lib/heap.rb
@@ -2,16 +2,21 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
-    @prc = prc ||= Proc.new{|el1, el2| el1 <=> el2 }
+    @prc = (prc ||= Proc.new do |el1, el2| 
+      el1 <=> el2
+    end)
     @store = []
   end
 
   def count
-    @store.length
+    @store.length - 1
   end
 
   def extract
     @store[0], @store[-1] = @store[-1], @store[0]
+    val = @store.pop
+    BinaryMinHeap.heapify_down(@store, 0)
+    val
   end
 
   def peek
@@ -20,14 +25,15 @@ class BinaryMinHeap
 
   def push(val)
     @store.push(val)
+    BinaryMinHeap.heapify_up(@store, count)
   end
 
   public
   def self.child_indices(len, parent_index)
     child_idx = [parent_index*2 + 1, parent_index*2 + 2]
     return child_idx if len > child_idx[1]
-    return nil if len < child_idx[0]
-    return [child_idx[0]]
+    return [] if len <= child_idx[0]
+    [child_idx[0]]
   end
 
   def self.parent_index(child_index)
@@ -36,17 +42,32 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
-    
-    children = BinaryMinHeap.child_indices(len, parent_idx)
-    until children.none? {|child| prc.call(array[child], array[parent_idx]) == }
-    return array if children.empty?
-    if children.length == 1 
-      case (prc.call(array[children[0]], array[parent_idx]))
+    prc ||= Proc.new{|el1, el2| el1 <=> el2 }
 
-      array[children[0]], array[parent_idx] = array[parent_idx], arr[children[0]]
+    children = BinaryMinHeap.child_indices(len, parent_idx)
+    until children.all? { |child| prc.call(array[child], array[parent_idx]) > -1 }
+      if children.length == 2 && (prc.call(array[children[1]],array[children[0]]) == -1 )
+        array[children[1]], array[parent_idx] = array[parent_idx], array[children[1]] 
+        parent_idx = children[1]
+      else
+        array[children[0]], array[parent_idx] = array[parent_idx], array[children[0]] 
+        parent_idx = children[0]
+      end
+      children = BinaryMinHeap.child_indices(len, parent_idx)
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
-
+    prc ||= Proc.new {|el1, el2| el1 <=> el2 }
+    return array if child_idx.zero?
+    parent = BinaryMinHeap.parent_index(child_idx)
+    until prc.call(array[parent], array[child_idx]) < 1 
+      array[child_idx], array[parent] = array[parent], array[child_idx]
+      child_idx = parent
+      break if child_idx.zero?
+      parent = BinaryMinHeap.parent_index(child_idx)
+    end
+    array
   end
 end
--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,5 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    pr = Proc.new{|el1, el2| el2 <=> el1 }
+    heap_boundary = 1
+    until heap_boundary == self.length
+      p self[0..heap_boundary]
+      BinaryMinHeap.heapify_up(self[0..heap_boundary],
+        heap_boundary, heap_boundary - 0,
+        &pr)
+      p self[0..heap_boundary]
+      heap_boundary += 1
+    end
+    self
   end
 end
